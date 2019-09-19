4d20c13cbcdcc06d25525e3a3ebb475b546feb8d
cecind123 <cecind123@gmail.com>
2019-09-19 18:16:55 -0400

    PyParagraph

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -1,24 +1,28 @@
-import os 
-file = "paragraph_3.txt"
+#import dependencies and txt file (paragraph_1.txt or paragraph_2.txt or paragraph_3.txt)
 
+import os 
+file = "paragraph_1.txt"
+
+#Create counters
 word_count = 0
 sentence_count = 0
 avg_letter_count = 0
 avg_sentence_length = 0
 
+#open file
 with open(file, "r") as txtfile:
     
     for line in txtfile:
         words = line.split()
-        word_count = word_count + len(words)
-        sentence_count = line.count('.') + line.count('!') + line.count('?')
-        avg_letter_count = ((avg_letter_count + len(line))- line.count(" "))/ word_count
+        word_count += + len(words)
+        sentence_count += line.count('.') + line.count('!') + line.count('?')
+        avg_letter_count +=  (+ len(line)- line.count(" "))/ word_count
         avg_sentence_length = word_count/sentence_count
 
-
+#Print results
 print("Paragraph Analysis")
 print("-------------------------")
 print("Approximate Word Count: {}".format(word_count))
 print("Approximate Sentence Count: {}".format(sentence_count))
 print("Average Letter Count: {:.1f}".format(avg_letter_count))
-print("Average Sentence Length: {}".format(avg_sentence_length))
+print("Average Sentence Length: {:.1f}".format(avg_sentence_length))
